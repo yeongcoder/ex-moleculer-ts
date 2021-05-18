23a3ac84e2b363f781ac66f8038d331d59b2e65f
유씨웨어개발팀 <seoyeongje@yussiweeogaebaltim-ui-MacBookPro.local>
2021-05-18 14:01:22 +0900

    npm scirpt 수정 및 services/*/index.ts 삭제

--- moleculer.config.ts
+++ moleculer.config.ts
@@ -59,7 +59,7 @@ const brokerConfig: BrokerOptions = {
 	// More info: https://moleculer.services/docs/0.14/networking.html
 	// Note: During the development, you don't need to define it because all services will be loaded locally.
 	// In production you can set it via `TRANSPORTER=nats://localhost:4222` environment variable.
-	transporter: null, // "TCP"
+	transporter: "TCP", // "TCP"
 
 	// Define a cacher.
 	// More info: https://moleculer.services/docs/0.14/caching.html
@@ -153,21 +153,21 @@ const brokerConfig: BrokerOptions = {
 
 	// Enable/disable built-in metrics function. More info: https://moleculer.services/docs/0.14/metrics.html
 	metrics: {
-		enabled: true,
+		enabled: false,
 		// Available built-in reporters: "Console", "CSV", "Event", "Prometheus", "Datadog", "StatsD"
 		reporter: {
 			type: "Prometheus",
 			options: {
-					// HTTP port
-					port: 3030,
-						// HTTP URL path
-						path: "/metrics",
-						// Default labels which are appended to all metrics labels
-						defaultLabels: (registry: MetricRegistry) => ({
-						namespace: registry.broker.namespace,
-						nodeID: registry.broker.nodeID,
-					}),
-				},
+				// HTTP port
+				port: 3030,
+					// HTTP URL path
+					path: "/metrics",
+					// Default labels which are appended to all metrics labels
+					defaultLabels: (registry: MetricRegistry) => ({
+					namespace: registry.broker.namespace,
+					nodeID: registry.broker.nodeID,
+				}),
+			},
 		},
 	},
 
